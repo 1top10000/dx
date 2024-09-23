4d4bc96b1df81b24cc3f04fc3f46e7e8d23a699e
1top10000 <107670523+1top10000@users.noreply.github.com>
2024-09-23 17:37:10 +0900

    Update at.js

--- at.js
+++ at.js
@@ -1,12 +1,40 @@
+function dc(object) {
+    if (object === null || typeof object !== "object") {
+        return object;
+    }
+    const copy = Array.isArray(object) ? [] : {};
+    for (let key of Object.keys(object)) {
+        copy[key] = dc(object[key]);
+    }
+    return copy;
+}
 const op = (_pr, _bc) => {
-    let _pr_ = _pr;
-    for (let gt in _bc) {
-        if (gt !== '1') {
-            if (_pr_[gt]) {
-                _pr_[gt].exp = _pr_[gt].exp + _bc[gt].exp;
-            } else {
-                _pr_[gt] = {exp:1};
-            }
+    let _pr_ = dc(_pr[0]);
+    let _bc_ = dc(_bc[0]);
+    for (let gt in _bc_) {
+        if (_pr_[gt]) {
+            _pr_[gt].exp = _pr_[gt].exp + _bc_[gt].exp;
+        } else {
+            _pr_[gt] = {exp:1};
         }
     }
+    return [_pr_, _pr[1] * _bc[1]];
+};
+const ap = (_b, g_) => {
+    let r0 = [];
+    for (let _i_ of _b) {
+        for (let _j_ of g_) {
+            r0[r0.length] = op(_i_, _j_);
+        }
+    }
+    return r0;
+};
+const ts = (__n__) => {
+    let rt = [];
+    for (let _i_ = 0; _i_ < __n__; _i_++) {
+        let tp = {};
+        tp["b"+Number(_i_)] = { exp: _i_ }
+        rt[rt.length] = [tp, undefined];
+    }
+    return rt;
 };
