f502c5f828d82b6285eef1c690932f41c09bdad5
1top10000 <107670523+1top10000@users.noreply.github.com>
2024-09-24 18:58:17 +0900

    Update at.js

--- at.js
+++ at.js
@@ -15,7 +15,7 @@ const op = (_pr, _bc) => {
         if (_pr_[gt]) {
             _pr_[gt].exp = _pr_[gt].exp + _bc_[gt].exp;
         } else {
-            _pr_[gt] = {exp:1};
+            _pr_[gt] = { exp: _bc_[gt].exp };
         }
     }
     return [_pr_, _pr[1] * _bc[1]];
@@ -33,8 +33,37 @@ const ts = (__n__) => {
     let rt = [];
     for (let _i_ = 0; _i_ < __n__; _i_++) {
         let tp = {};
-        tp["b"+Number(_i_)] = { exp: _i_ }
-        rt[rt.length] = [tp, undefined];
+        if (_i_ !== 0) {
+            tp.x = { exp: _i_ };
+        }
+        tp["b" + Number(_i_)] = { exp: 1 };
+        rt[rt.length] = [tp, 1];
     }
     return rt;
+};
+const xta = (__a, _q) => {
+    let _a = [];
+    for (let _i_ of __a) {
+        console.log(_i_);
+        if (_i_[0].x) {
+            console.log('L--');
+            let tlp = dc(_i_);
+            let oro = tlp[0].x.exp;
+            let _q__ = dc(_q);
+            console.log(tlp, oro, _q__);
+            if (oro != 0) {
+                for (let i = 0; i < (oro - 1); i++) {
+                    _q__ = ap(_q__, _q);
+                }
+            }
+            console.log(_q__);
+            delete tlp[0].x;
+            console.log(tlp);
+            _a = _a.concat(ap([tlp], _q__));
+            console.log(ap([tlp], _q__));
+        } else {
+            _a = _a.concat([_i_]);
+        }
+    }
+    return _a;
 };
