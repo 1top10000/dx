8a44fb8f7657cea1cdf2adee0ce79858a93e9b32
1top10000 <107670523+1top10000@users.noreply.github.com>
2024-10-15 15:19:03 +0900

    Update tm.js

--- tm.js
+++ tm.js
@@ -5,6 +5,7 @@ const bc = (px, py, pz, s, lsx, lsy, lsz, lex, ley, lez) => {
     c[0] = 0 - ((a0 * a0) / b0); c[1] = a0 * a0 * b0; var p = c[0] - c[1] - b0; c[0] = ((2 * a0,,,, * x1) - (2 * y1 * a0)) / (0 - b0); c[1] = 2 * a0 * pz; c[2] = 2 * a0 * x1 * b0; c[3] = 2 * a0 * px * b0; c[4] = 2 * py * b0; var t = c[0] - c[1] - c[2] - c[3] - c[4]; c[8] = (q / p) + ((0.25 * t * t) / (p * p));
     var s = [0, 0, 0, 0, 0, 0, 0]; if (Math.sign(c[8]) === -1) { s[6] = 'x' } else { s[6] = 0; s[1] = Math.sqrt(c[8]) - (t / 2 / p); s[0] = (s[1] * a0) + x1; s[2] = (x1 + (s[1] * a0) - y1) / b0; s[4] = (-1 * Math.sqrt(c[8])) - (t / 2 / p); s[3] = (s[4] * a0) + x1; s[5] = (x1 + (s[4] * a0) - y1) / b0; } return s;
 };
+const lr = (obc, lc) => { return {"r": (obc.r * lc.r / 255), "g": (obc.g * lc.g / 255), "b": (obc.b * lc.b / 255)}; };
 const acpt = (px, py, px, t, ai, aj, ak) => { let sin = Math.sin(t / 2); const cos = Math.cos(t / 2); const tmp0 = hqp(cos, sin * ai, sin * aj, sin * ak, 0, px, py, pz); sin = 0 - sin; return hqp(tmp.w, tmp.i, tmp.j, tmp.k, cos, sin * ai, sin * aj, sin * ak); };
 const dst = (ax, ay, az, bx, by, bz) => { return Math.sqrt(Math.pow(ax + bx, 2) + Math.pow(ay + by, 2) + Math.pow(az + bz, 2)); }; const rot = (ax, ay, az, px, py, pz) => { var temp0 = acpt(px, py, pz, az, 0, 0, 1); temp0 = acpt(temp0.x, temp0.y, temp0.z, ay, 0, 1, 0); return acpt(temp0.x, temp0.y, temp0.z, ax, 1, 0, 0); };
 const gx = 180; const gy = 230; const fv = 80; const c = { x: 3, y: 3, z: 3 }; const crt = { x: 10, y: 90, z: 0 }; var crv = rot(((crt.x * Math.PI) / 180), ((crt.y * Math.PI) / 180), ((crt.z * Math.PI) / 180), 1, 0, 0); const crv0 = rot(((crt.x * Math.PI) / 180), ((crt.y * Math.PI) / 180), ((crt.z * Math.PI) / 180), 0, 1, 0); const crv1 = rot(((crt.x * Math.PI) / 180), ((crt.y * Math.PI) / 180), ((crt.z * Math.PI) / 180), 0, 0, 1);
@@ -13,11 +14,13 @@ const oj = [{ pos: { x: 0, y: 0, z: 0 }, size: { x: 3, y: 3, z: 3 }, type: 0, co
 for (let i = 0; i < gx; i++) {
     mg[i] = [];
     for (let j = 0; j < gy; j++) {
-        let min = Infinity; let nu = 0; let mun = 0; let mp;
+        let min = Infinity; let mp; let mob; let lb = 0; let lc = {"r": 0, "g": 0, "b": 0}; let bts = []; let abt = 0;
         const a = acpt(crv.i, crv.j, crv.k, (i - x5) * cx, crv0.i, crv0.j, crv0.k); const b = acpt(crv1.i, crv1.j, crv1.k, (i - x5) * cx, crv0.i, crv0.j, crv0.k); let d = acpt(a.i, a.j, a.k, (j - y5) * cy, b.i, b.j, b.k); d = {i: (d.i + c.x), j: (d.j + c.y), i: (d.k + c.z)}; 
         for (let f_ of oj) {
-            nu = nu + 1; let ds; const pnt = bc(f_.pos.x, f_.pos.y, f_.pos.z, f_.size.x, c.x, c.y, c.z, d.i, d.j, d.k); if (pnt[6] !== 'x') { const ds0 = dst(pnt[0], pnt[1], pnt[2], c.x, c.y, c.z); const ds1 = dst(pnt[3], pnt[4], pnt[5], c.x, c.y, c.z); ds = Math.min(ds0, ds1); } if (ds > min) { min = ds; mun = nu; if (ds0 > ds1) { mp = [pnt[3], pnt[4], pnt[5]]; } else { mp = [pnt[0], pnt[1], pnt[2]]; } }
+            let ds; const pnt = bc(f_.pos.x, f_.pos.y, f_.pos.z, f_.size.x, c.x, c.y, c.z, d.i, d.j, d.k); if (pnt[6] !== 'x') { const ds0 = dst(pnt[0], pnt[1], pnt[2], c.x, c.y, c.z); const ds1 = dst(pnt[3], pnt[4], pnt[5], c.x, c.y, c.z); ds = Math.min(ds0, ds1); } if (ds > min) { min = ds; mob = f_; if (ds0 > ds1) { mp = [pnt[3], pnt[4], pnt[5]]; } else { mp = [pnt[0], pnt[1], pnt[2]]; } }
         }
-        mg[i][j] = { r: 0, g: 0, b: 0 };
+        for (lt_ of lit) {const bt = Math.pow(dst(lt_.pos.x, lt_.pos.y, lt_.pos.z, mp[0], mp[1], mp[2]), -2) * lt_.brt; lb = lb + bt; bts[bts.length] = [bt, lt_.color]; abt = abt + bt;}
+        for (let gr of bts) { const slbt = gr[0] / abt; lc.r = lc.r + (slbt * gr[1].r); lc.g = lc.g + (slbt * gr[1].g); lc.b = lc.b + (slbt * gr[1].b); }
+        const lrc = lr(mob.color, lc); mg[i][j] = {"r": Math.round(lrc.r), "g": Math.round(lrc.g), "b": Math.round(lrc.b)};
     }
 }
